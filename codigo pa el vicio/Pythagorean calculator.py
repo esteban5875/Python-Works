b7f477845b3a9c6efe68ac7030fa1a712a146523
esteban <99847900+esteban5875@users.noreply.github.com>
2023-10-31 22:22:24 -0400

    Update Pythagorean calculator.py

--- codigo pa el vicio/Pythagorean calculator.py
+++ codigo pa el vicio/Pythagorean calculator.py
@@ -1,11 +1,8 @@
-import sys
 import math
+import sys
 
 def pythagorean_(a, b):
-    c = math.sqrt(a**2 + b**2)
-
-def pythagorean_case2(a, b):
-    c = math.sqrt(a**2 + b**2)
+    return math.sqrt(a**2 + b**2)
 
 while True:
     val1 = input("Enter hypothenuse --optional--: ")
@@ -14,8 +11,16 @@ while True:
     max_num = sys.float_info
 
     try:
-        if (val1 == "") and (val3 == ""):
+        if (val1 == "" and val3 == "") or (val2 == ""):
             print("\n Error \n")
         else:
-            if (val1 == float(val2)) and (val2 == float(val2)):
-                pythagorean_case2()
+            if val1 != "":
+                result = pythagorean_(float(val1), float(val2))
+                print(f"\n The other leg is: {round(result, 10)} \n")
+            else:
+                result = pythagorean_(float(val2), float(val3))
+                print(f"\n The hypothenuse: {round(result, 10)} \n")
+    except ValueError:
+        print("\n Invalid Input Error \n")
+    except OverFlowError: 
+        print(f"\n Result too large...Limit is: {max_num} \n")
