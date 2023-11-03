d5fbf1cabf881bdf987393ac7dee3056e73342bd
esteban <99847900+esteban5875@users.noreply.github.com>
2023-11-03 14:40:01 -0400

    Update Inventario.py

--- codigo pa el vicio/Inventario.py
+++ codigo pa el vicio/Inventario.py
@@ -28,6 +28,8 @@ while True:
             product_name = input("What product are you adding: ").lower()
             if isinstance(product_name, float):
                 print(error)
+            elif product_name in product_list:
+                print(f"{error} --Use update to change product already in list--") 
             else:
                 price = float(input("What is the price (no dollar sign): "))
                 quantity = int(input("What quantity (just number): "))
@@ -37,7 +39,7 @@ while True:
                 price_list.append(price)
         elif user_action == "display":
             for item, price, quantity, total in zip(product_list, price_list, quantity_list, total_price_list):
-                print(f"Product: {item} x {quantity}...Product Price: {price}...Total Price: {round(total, 3)}")
+                print(f"Product: {item} x {quantity}...Product Price: {price}...Revenue: {round(total, 3)}")
         elif user_action == "delete":
             deletion = str(input("What are you deleting: ")).lower()
             if deletion not in product_list:
@@ -54,19 +56,26 @@ while True:
                     print("Alright")
         elif user_action == "update":
             product_selection = str(input("Which: ")).lower()
+            update_option = str(input("What property to update: ")).lower()
             if product_selection not in product_list:
                 print(error)
-            else:
-                new_name = str(input("Enter new product: ")).lower()
-                new_quantity = int(input("Enter new product quantity: "))
-                new_price = int(input("Enter new price: "))
+            elif update_option == "name":
+                new_name = str(input("Enter new product name: ")).lower()
                 index_to_update = product_list.index(product_selection)
                 product_list.pop(index_to_update)
+                product_list.append(new_name)
+            elif update_option == "quantity":    
+                index_to_update = product_list.index(product_selection)
+                new_quantity = int(input("Enter new product quantity: "))
                 quantity_list.pop(index_to_update)
+                quantity_list.append(new_quantity)
+            elif update_option == "price":    
+                index_to_update = product_list.index(product_selection)
+                new_price = int(input("Enter new price: "))
                 price_list.pop(index_to_update)
-                quantity_list.insert(index_to_update, new_quantity)
-                product_list.insert(index_to_update, new_name)
-                price_list.insert(index_to_update, new_price)
+                price_list.append(new_price)
+                total_price_list.pop(index_to_update)
+                total_price_list.append(new_price*quantity)
         elif user_action == "reset":
             confirmation_ = input("Are you sure ? : ").lower()
             if confirmation_ == "yes":
@@ -79,7 +88,5 @@ while True:
                 print(error)
         elif user_action == "clear":
             os.system('clear')
-            
-
     except ValueError:
         print(error)
