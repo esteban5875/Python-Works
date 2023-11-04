b5a42fe9afb09f1b594c57632d8250a18bc3f06d
esteban <99847900+esteban5875@users.noreply.github.com>
2023-11-03 20:57:48 -0400

    Cambios

--- codigo pa el vicio/Inventario.py
+++ codigo pa el vicio/Inventario.py
@@ -19,17 +19,21 @@ price_list = []
 quantity_list = []
 total_price_list = []
 
+def passing():
+    pass
+
 while True:
     user_action = str(input(">>> ")).lower()
-    error = "Oops! An error occurred while processing your request. Please check your input and try again."
-
+    os_name = os.name
     try:
-        if user_action == "add":
+        if user_action == "exit":
+            break
+        elif user_action == "add":
             product_name = input("What product are you adding: ").lower()
-            if isinstance(product_name, float):
-                print(error)
-            elif product_name in product_list:
-                print(f"{error} --Use update to change product already in list--") 
+            if product_name in product_list:
+                print(f"Item already in list --Use update to change product already in list--")
+            elif product_name == "":
+                passing()
             else:
                 price = float(input("What is the price (no dollar sign): "))
                 quantity = int(input("What quantity (just number): "))
@@ -39,7 +43,7 @@ while True:
                 price_list.append(price)
         elif user_action == "display":
             for item, price, quantity, total in zip(product_list, price_list, quantity_list, total_price_list):
-                print(f"Product: {item} x {quantity}...Product Price: {price}...Revenue: {round(total, 3)}")
+                print(f"Product: {item.capitalize()} x {quantity}...Product...Price: {price}$...Revenue: {round(total, 3)}$")
         elif user_action == "delete":
             deletion = str(input("What are you deleting: ")).lower()
             if deletion not in product_list:
@@ -54,28 +58,34 @@ while True:
                     product_list.pop(index_to_del)
                 elif confirmation.lower() == "no":
                     print("Alright")
+                else:
+                    passing()
         elif user_action == "update":
             product_selection = str(input("Which: ")).lower()
-            update_option = str(input("What property to update: ")).lower()
             if product_selection not in product_list:
-                print(error)
-            elif update_option == "name":
-                new_name = str(input("Enter new product name: ")).lower()
-                index_to_update = product_list.index(product_selection)
-                product_list.pop(index_to_update)
-                product_list.append(new_name)
-            elif update_option == "quantity":    
-                index_to_update = product_list.index(product_selection)
-                new_quantity = int(input("Enter new product quantity: "))
-                quantity_list.pop(index_to_update)
-                quantity_list.append(new_quantity)
-            elif update_option == "price":    
-                index_to_update = product_list.index(product_selection)
-                new_price = int(input("Enter new price: "))
-                price_list.pop(index_to_update)
-                price_list.append(new_price)
-                total_price_list.pop(index_to_update)
-                total_price_list.append(new_price*quantity)
+                passing()
+            else:
+                update_option = str(input("What property to update: ")).lower()
+                if update_option not in ["name", "quantity", "price"]:
+                    passing()
+                else:    
+                    if update_option == "name":
+                        new_name = str(input("Enter new product name: ")).lower()
+                        index_to_update = product_list.index(product_selection)
+                        product_list[index_to_update] = new_name
+                    elif update_option == "quantity":
+                        index_to_update = product_list.index(product_selection)
+                        new_quantity = int(input("Enter new product quantity: "))
+                        quantity_list[index_to_update] = new_quantity
+                        total_price_case = price_list[index_to_update] * new_quantity
+                        total_price_list[index_to_update] = total_price_case 
+                    elif update_option == "price":    
+                        index_to_update = product_list.index(product_selection)
+                        new_price = float(input("Enter new price: "))
+                        price_list[index_to_update] = new_price
+                        quantity_case = quantity_list[index_to_update]
+                        total_price_case = float(new_price) * float(quantity_case)
+                        total_price_list[index_to_update] = total_price_case
         elif user_action == "reset":
             confirmation_ = input("Are you sure ? : ").lower()
             if confirmation_ == "yes":
@@ -83,10 +93,22 @@ while True:
                 price_list.clear()
                 quantity_list.clear()
             elif confirmation_ == "no":
+                print("       ")
                 print("Alright")
+                print("       ")
             else:
-                print(error)
+                passing()
         elif user_action == "clear":
-            os.system('clear')
+            if os_name == "posix":
+                os.system('clear')
+            elif os_name == "nt":
+                os.system('cls')
+            else:
+                print("Your OS was not defined, --clear-- only works for Windows or Unix/Linux Systems")
+        elif user_action == "export":
+            name_file = "Name your file --No extensions, will be saved to Documents-- : "
+            with open(f"C:\\Users\\User\\Documents\\{name_file}.txt", "x") as b:
+                for item, price, quantity, total in zip(product_list, price_list, quantity_list, total_price_list):
+                    b.write(f"Product: {item} x {quantity}...Price: {price}...Revenue: {total}")
     except ValueError:
-        print(error)
+        passing()
