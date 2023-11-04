f2476ea3375f8de55a7ee49fc27a82dcf31a1636
esteban <99847900+esteban5875@users.noreply.github.com>
2023-11-04 12:14:31 -0400

    Cambios

--- codigo pa el vicio/Gestor.py
+++ codigo pa el vicio/Gestor.py
@@ -11,7 +11,7 @@ If the user input "update", the code will allow the user to update an item from 
 If the user input "reset" the code will reset all lists.
 If the user input "clear" console is cleared.
 If the user input "export", then an option to export as .txt will be used.
-If there is a ValueError, the code will print "Oops! An error occurred while processing your request. Please check your input and try again."
+If there is a ValueError, the code will print "Oops! An error occurred while processing your request. Please check your input and try again.
 '''
 import os
 
@@ -32,27 +32,27 @@ while True:
         if user_action == "exit":
             break
         elif user_action == "add":
-            product_name = input("What product are you adding: ").lower()
+            product_name = input("\nWhat product are you adding: ").lower()
             if product_name in product_list:
-                print(f"Item already in list --Use update to change product already in list--")
+                print(f"\nItem already in list --Use update to change product already in list--\n")
             elif product_name == "":
                 passing()
             else:
-                price = float(input("What is the price (no dollar sign): "))
-                quantity = int(input("What quantity (just number): "))
+                price = float(input("\nWhat is the price (no dollar sign): "))
+                quantity = int(input("\nWhat quantity (just number): "))
                 product_list.append(product_name)
                 quantity_list.append(quantity)
                 total_price_list.append(price*quantity)
                 price_list.append(price)
         elif user_action == "display":
             for item, price, quantity, total in zip(product_list, price_list, quantity_list, total_price_list):
-                print(f"Product: {item.capitalize()} x {quantity}...Product...Price: {price}$...Revenue: {round(total, 3)}$")
+                print(f"\nProduct: {item.capitalize()} x {quantity}...Product...Price: {price}$...Revenue: {round(total, 3)}$\n")
         elif user_action == "delete":
-            deletion = str(input("What are you deleting: ")).lower()
+            deletion = str(input("\nWhat are you deleting: ")).lower()
             if deletion not in product_list:
-                print("Item not found")
+                print("\nItem not found\n")
             else:
-                confirmation = str(input("Are you sure ? : ")).lower()
+                confirmation = str(input("\nAre you sure ? : ")).lower()
                 if confirmation.lower() == "yes":
                     index_to_del = product_list.index(deletion)
                     price_list.pop(index_to_del)
@@ -64,41 +64,39 @@ while True:
                 else:
                     passing()
         elif user_action == "update":
-            product_selection = str(input("Which: ")).lower()
+            product_selection = str(input("\nWhich: ")).lower()
             if product_selection not in product_list:
                 passing()
             else:
-                update_option = str(input("What property to update: ")).lower()
+                update_option = str(input("\nWhat property to update: ")).lower()
                 if update_option not in ["name", "quantity", "price"]:
                     passing()
                 else:    
                     if update_option == "name":
-                        new_name = str(input("Enter new product name: ")).lower()
+                        new_name = str(input("\nEnter new product name: ")).lower()
                         index_to_update = product_list.index(product_selection)
                         product_list[index_to_update] = new_name
                     elif update_option == "quantity":
                         index_to_update = product_list.index(product_selection)
-                        new_quantity = int(input("Enter new product quantity: "))
+                        new_quantity = int(input("\nEnter new product quantity: "))
                         quantity_list[index_to_update] = new_quantity
                         total_price_case = price_list[index_to_update] * new_quantity
                         total_price_list[index_to_update] = total_price_case 
                     elif update_option == "price":    
                         index_to_update = product_list.index(product_selection)
-                        new_price = float(input("Enter new price: "))
+                        new_price = float(input("\nEnter new price: "))
                         price_list[index_to_update] = new_price
                         quantity_case = quantity_list[index_to_update]
                         total_price_case = float(new_price) * float(quantity_case)
                         total_price_list[index_to_update] = total_price_case
         elif user_action == "reset":
-            confirmation_ = input("Are you sure ? : ").lower()
+            confirmation_ = input("\nAre you sure ? : ").lower()
             if confirmation_ == "yes":
                 product_list.clear()
                 price_list.clear()
                 quantity_list.clear()
             elif confirmation_ == "no":
-                print("       ")
-                print("Alright")
-                print("       ")
+                print("\nAlright\n")
             else:
                 passing()
         elif user_action == "clear":
@@ -107,15 +105,11 @@ while True:
             elif os_name == "nt":
                 os.system('cls')
             else:
-                print("Your OS was not defined, --clear-- only works for Windows or Unix/Linux Systems")
+                print("\nYour OS was not defined, --clear-- only works for Windows or Unix/Linux Systems\n")
         elif user_action == "export":
-            name_file = input("Name your file and path --No extensions-- : ")
+            name_file = input("\nName your file --No extensions-- : ")
             with open(fr"{name_file}.txt", "x") as b:
-                for item, price, quantity, total in zip(product_list, price_list, quantity_list, total_price_list):  
-                    b.write(f"Product: {item} x {quantity}...Price: {price}...Revenue: {total}")
-        elif user_action == "import":
-            file_option = input("Which file to input: ")
-            
-            
+                for item, price, quantity, total in zip(product_list, price_list, quantity_list, total_price_list):
+                    b.write(f"\nProduct: {item} x {quantity}...Price: {price}...Revenue: {total}\n")
     except ValueError:
         passing()
