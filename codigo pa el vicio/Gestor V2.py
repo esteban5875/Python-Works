03fc7b909086f697be34eea6781277f2b1f65123
esteban <99847900+esteban5875@users.noreply.github.com>
2023-11-06 15:10:50 -0400

    Update Gestor V2.py

--- codigo pa el vicio/Gestor V2.py
+++ codigo pa el vicio/Gestor V2.py
@@ -3,78 +3,78 @@ import os
 general_dict = {}
 
 def add_to_dict():
-    list_name = input("Enter name of the section: ").lower()
+    list_name = input("\nEnter name of the section: ").lower()
     
     if list_name in general_dict:
-        print("Section already in Inventory")
+        print("\nSection already in Inventory\n")
     else:
         general_dict[list_name] = []
 
 def  add_to_list():
-    key = input("In which section ? : ").lower()
+    key = input("\nIn which section ? : ").lower()
     if key not in general_dict:
-        print("Section does not exist")
+        print("\nSection does not exist\n")
     else:
-        product_name = input("What is the name of the product ? : ").lower()
-        price = float(input("What is the price ? : "))
-        quantity = int(input("What is the quantity ? : "))
+        product_name = input("\nWhat is the name of the product ? : ").lower()
+        price = float(input("\nWhat is the price ? : "))
+        quantity = int(input("\nWhat is the quantity ? : "))
         product = (product_name, price, quantity)
         general_dict[key].append(product)
         
 def update_product_price():
     product_section = input("Which section ? : ").lower()
     if product_section not in general_dict:
-        print("Section not found")
+        print("\nSection not found\n")
     else:
-        product_name = input("What product to update ? : ").lower()
+        product_name = input("\nWhat product to update ? : ").lower()
         found = False
         for product in general_dict[product_section]:
             if product[0] == product_name:
                 found = True
         if found == True:
-            print("Price updated succesfully")
-            new_price = float(input("Enter new price: "))
+            print("\nPrice updated succesfully\n")
+            new_price = float(input("\nEnter new price: "))
             product = (product[0], new_price, product[2])
         else:
-            print("Product not found")   
+            print("\nProduct not found\n")   
 
 def update_product_name():
-    product_section = input("What section ? : ").lower()
+    product_section = input("\nWhat section ? : ").lower()
     if product_section not in general_dict:
-        print("Section not found")
+        print("\nSection not found\n")
     else:
-        product_name = input("What product to update ? : ").lower()
+        product_name = input("\nWhat product to update ? : ").lower()
         found = False
         for product in general_dict[product_section]:
             if product[0] == product_name:
                 found = True
         if found == True:
-            new_name = input("Enter new name : ").lower()
+            new_name = input("\nEnter new name : ").lower()
             new_product_name = (new_name, product[1], product[2])
-            print("Product updated succesfully")
+            print("\nProduct updated succesfully\n")
         else:
-            print("Product not found")    
+            print("\nProduct not found\n")    
 
 def update_product_quantity():
-    product_section = input("What section ? : ").lower()
+    product_section = input("\nWhat section ? : ").lower()
     if product_section not in general_dict:
-        print("Section not found")
+        print("\nSection not found\n")
     else:
-        product_name = input("What product to update ? : ").lower()
+        product_name = input("\nWhat product to update ? : ").lower()
         found = False
         for product in general_dict[product_section]:
             if product[0] == product_name:
                 found = True
         if found == True:
-            new_quantity = int(input("Enter new quantity: "))
+            new_quantity = int(input("\nEnter new quantity: "))
             quantity_new = (product[0], product[1], new_quantity)
         else:
-            print("Product not found")    
+            print("\nProduct not found\n")    
 
 def display_products():
-    section_display = input("In which section ? : ").lower()
+    section_display = input("\nIn which section ? : ").lower()
     if section_display not in general_dict:
-        print("Section not found")
+        print("\nSection not found\n")
         list = general_dict[section_display]
     else:
         for product in general_dict[section_display]:
@@ -82,7 +82,7 @@ def display_products():
             price = product[1]
             quantity = product[2]
             revenue = price * quantity
-            print(f"Product: {name} x {quantity}..Price: {price}...Revenue: {revenue} \n")
+            print(f"\nProduct: {name} x {quantity}..Price: {price}...Revenue: {revenue}\n")
             
 def clear():
   os_name = os.name
@@ -91,13 +91,13 @@ def clear():
   elif os_name == 'posix':
       os.system('clear')
   else:
-      print("Clear command only for linux/unix and windows systems, your os was not identified")    
+      print("\nClear command only for linux/unix and windows systems, your os was not identified\n")    
 
     
 def display_sections():
     
     for sections in general_dict:
-        print(f"\n{sections.capitalize()}")
+        print(f"\n{sections.capitalize()}\n")
 
 def export_():
     file_name = input(r"Name your file --Only txt is allowed for extension: ")
@@ -105,30 +105,59 @@ def export_():
         for section in general_dict:
             yield section
     sections = loop_iteration()
-    with open(f"{file_name.capitalize()}.txt", "w") as b:
+    with open(fr"{file_name.capitalize()}.txt", "w") as b:
         for product in sections:
          name = product[0]
          price = product[1]
          quantity = product[2]
          revenue = price * quantity
-         b.write(f"Product: {name.capitalize()} x {quantity}..Price: {price}...Revenue: {revenue}\n")
+         b.write(f"\nProduct: {name.capitalize()} x {quantity}..Price: {price}...Revenue: {revenue}\n")
 
 def passing():
     pass        
         
+def delete_sections():
+    section_name = input("\nEnter section name: ").lower()
+    if section_name not in general_dict:
+        print("\nSection not found\n")
+    else:
+        general_dict.pop(section_name)
+    
+def delete_products():
+    section_name = input("\nIn which section ? : ").lower()
+    if section_name not in general_dict:
+        print("\nSection not found\n")
+    else:    
+         product_name = input("\nEnter product to delete: ").lower()
+         for product in general_dict[section_name]:
+            if product[0] == product_name:
+                section_name.pop(product[0], product[1], product[2])
+            else:
+                print("\nProduct not found\n")
+             
+def reset_():
+    confirmation = input("Are you sure ? (yes/no) : ").lower()
+    if confirmation == "yes":
+        main_dict.clear()
+        print("\nReset succesful\n")
+    elif confirmation == "no":
+        print("\nReset cancelled...\n")
+    else:
+        return                          
+                                    
 while True:
-    user_action = input(">>> ").lower()
+    user_action = input("\n>>> ").lower()
     if user_action == "exit":
         break
     try:
         if user_action == "create section":
             add_to_dict()
-        elif user_action == "in section add" :
+        elif user_action == "in section add product" :
             add_to_list()
         elif user_action == "display sections":
            display_sections()
         elif user_action == "in section display products":
-            display_sections()
+            display_products()
         elif user_action == "in section update name":
                 update_product_name()
         elif user_action == "in section update price":
@@ -139,12 +168,18 @@ while True:
             clear()
         elif user_action == "export":
             export_()
+        elif user_action == "delete section":
+            delete_sections()
+        elif user_action == "in section delete product":
+            delete_products()
+        elif user_action == "reset data":
+            reset_()    
         else:
             passing()
     except ValueError:
-        print("Oops ! Value error, check your input and try again") 
+        print("\nOops ! Value error, check your input and try again\n") 
     except FileNotFoundError:
-        print("File especified not found")    
+        print("\nFile especified not found\n")
 
     
     
