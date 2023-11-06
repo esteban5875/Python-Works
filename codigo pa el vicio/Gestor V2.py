7b3444650024b378bab26919d1adb6eb16bf0307
esteban <99847900+esteban5875@users.noreply.github.com>
2023-11-05 22:18:57 -0400

    Update Gestor V2.py

--- codigo pa el vicio/Gestor V2.py
+++ codigo pa el vicio/Gestor V2.py
@@ -13,14 +13,32 @@ def add_to_dict():
 def  add_to_list():
     key = input("In which section ?: ")
     if key not in general_dict:
-        "Section not found"
+        print("Section does not exist")
     else:
         product_name = input("What is the name of the product ? : ")
         price = float(input("What is the price ? : "))
         quantity = int(input("What is the quantity ? : "))
-        product = ("", "", "")
+        product = (product_name, price, quantity)
         general_dict[key].append(product)
-
+        
+def update_product_price():
+    product_section = input("Which section ? : ").lower()
+    if sections not in general_dict:
+        print("Section not found").lower()
+    else:
+        product_name = input("What product to update ? : ").lower()
+        found = False
+        for product in general_dict[sections]:
+            if product[0] == products:
+                new_price = float(input("Enter new price: "))
+                product = (product[0], new_price, product[2])
+                product_found = True
+            else:
+                found = False
+        if found == True:
+            print("Price updated succesfully")
+        else:
+            print("Product not found")    
 
 
     
