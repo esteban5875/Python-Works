3a0c0c9b25b2ea1d5474ef616319032f7502cfc0
esteban <99847900+esteban5875@users.noreply.github.com>
2023-11-12 16:16:10 -0400

    sdfghjk

--- calculadora_POO.py
+++ calculadora_POO.py
@@ -1,105 +1,180 @@
-from math import cos, sin, tan
-class Math:
+import math
+
+class Sine:
     def __init__(self):
+       self.result = 0
+       try:
+            self.value = float(input("Enter value to find sine: "))
+       except ValueError as e:
+            e = "Oops ! There seems to be a value error, check your input and try again"
+            print(e)
+    def find_sine(self):
+        try:    
+            self.result = math.sin(self.value)
+            return round(self.result, 10)
+        except Exception as e:
+            e = "There seems to be an error, check your inputs and try again"
+            print(e)
 
+class Cosine:
+    def __init__(self):
+        self.result = 0
         try:
-            self.op = input("Enter Operator: ").lower()
-        except ValueError:
-            print("Value Error")
-        self.val = None
-        self.cos = None
-        self.sin = None
-        self.tan = None
-        self.sum = None
-        self.difference = None
-        self.product = None
-        self.coefficient = None
-        self.num1 = 0
-        self.num2 = 0
+            self.value = float(input("Enter a value to find cosine: "))
+        except ValueError as e:
+            e = "Oops ! There seems to be a value error, check your input and try again"
+            print(e)
+    def find_cosine(self):
+        try:    
+            self.result = math.cos(self.value)
+            return round(self.result, 10)
+        except Exception as e:
+            e = "There seems to be an error, check your inputs and try again"
+            print(e)
+
+class Tan:
+    def __init__(self):
+        self.result = 0
+        try:
+            self.value = float(input("Enter a value to find tangent: "))
+        except ValueError as e:
+            e = "Oops ! There seems to be a value error, check your input and try again"
+            print(e)
+    def find_tangent(self):
+        try:
+            self.result = math.tan(self.value)
+            return round(self.result, 10)
+        except Exception as e:
+            e = "There seems to be an error, check your inputs and try again"
+            print(e)
+
+class Sum:
+    def __init__(self):
+        self.result = 0
+        try:
+            self.val1 = float(input("Enter first value: "))
+        except ValueError as e:
+            e = "Oops ! There seems to be a value error, check your input and try again"
+            print(e)
         
-    
-
-        if self.op not in ["+", "-", "/", "*", "sin", "cos", "tan", "bye"]:
-            print("")
-            print("Operator not valid")
-            print("")
-        elif self.op == "cos":
-            try:
-                self.val = float(input("Enter value to find cosine: "))
-            except ValueError:
-                print("Enter a valid input")
-            self.cos = cos(self.val) 
-        elif self.op == "tan":
-            try:
-                self.val = float(input("Enter value to find tangent: "))
-            except ValueError as e:
-                print(e)
-            self.tan = tan(self.val)
-        elif self.op == "sin":
-            try:
-                self.val = float(input("Enter a value to find sine: "))
-            except ValueError as e:
-                print(e)
-            self.sin = sin(self.val)
-        else:
-            try:
-                self.num1 = float(input("Enter 1st value: "))
-            except ValueError as e:
-                e = "Value Error"
-                print(e)
-            try:  
-                self.num2 = float(input("Enter 2nd Value: "))
-            except ValueError as e:
-                e = "Value Error"
-                print(e)  
-              
+        try:
+            self.val2 = float(input("Enter second value: "))
+        except ValueError as e:
+            e = "Oops ! There seems to be a value error, check your input and try again"
+            print(e)
     def add(self):
-        self.sum = round(self.num1 + self.num2, 10)
-        if self.sum == 0:
-            pass
-        else:
-            return self.sum
+        try:
+            self.result = float(round(self.val1 + self.val2, 10))
+            return self.result
+        except Exception as e:
+            e = "There seems to be an error, check your inputs and try again"
+            print(e)
+
+class Diff:
+    def __init__(self):
+        self.result = 0
+        try:
+            self.val1 = float(input("Enter first value: "))
+        except ValueError as e:
+            e = "Oops ! There seems to be a value error, check your input and try again"
+            print(e)
+        
+        try:
+            self.val2 = float(input("Enter second value: "))
+        except ValueError as e:
+            e = "Oops ! There seems to be a value error, check your input and try again"
+            print(e)
     def substract(self):
-        self.difference = round(self.num1 - self.num2, 10)
-        if self.difference == 0:
-            pass
-        else:
-            return self.difference
-        return self.difference
-    def divide(self):
-        if self.num2 == 0:
-            print("Cannot divide by zero")
-        else:
-            self.coefficient = round(self.num1 / self.num2, 10)
-            if self.coefficient == 0:
-                pass
-            else:  
-                return self.coefficient
+        try:
+            self.result = float(round(self.val1 - self.val2))
+            return self.result
+        except Exception as e:
+            e = "There seems to be an error, check your inputs and try again"
+            print(e)
+            
+class Product:
+    def __init__(self):
+        self.result = 0
+        try:
+            self.val1 = float(input("Enter first value: "))
+        except ValueError as e:
+            e = "Oops ! There seems to be a value error, check your input and try again"
+            print(e)
+        
+        try:
+            self.val2 = float(input("Enter second value: "))
+        except ValueError as e:
+            e = "Oops ! There seems to be a value error, check your input and try again"
+            print(e)
     def multiply(self):
-        self.product = round(self.num1 * self.num2, 10)
-        if self.product == 0:
-            pass
-        else:
-            return self.product
+        try:
+            self.result = float(round(self.val1 * self.val2, 10))
+            return self.result
+        except Exception as e:
+            e = "There seems to be an error, check your inputs and try again"
+            print(e)
+
+class Div:
+    def __init__(self):
+        self.result = 0
+        try:
+            self.val1 = float(input("Enter first value: "))
+        except ValueError as e:
+            e = "Oops ! There seems to be a value error, check your input and try again"
+            print(e)
+        
+        try:
+            self.val2 = float(input("Enter second value: "))
+        except ValueError as e:
+            e = "Oops ! There seems to be a value error, check your input and try again"
+            print(e)
+    def divide(self):
+        try:
+            self.result = float(round(self.val1 / self.val2, 10))
+            return self.result
+        except ZeroDivisionError as b:
+            b = "Cannot divide by zero"
+            print(b)
+        except Exception as e:
+            e = "There seems to be an error, check your inputs and try again"
+            print(e)
     
-while True:
-    math_calculator = Math()
+class sqroot:
+    def __init__(self):
+        self.result = 0
+        try:
+            self.val = float(input("Enter value to find square root: "))
+        except ValueError as e:
+            e = "Oops ! There seems to be a value error, check your input and try again"
+            print(e)
+    def sqr(self):
+        try:
+            self.result = math.sqrt(self.val)
+            return round(self.result, 10)
+        except Exception as e:
+            e = "Oops ! There seems to be a value error, check your input and try again"
+            print(e)
+            
+class Exponent:
+    def __init__(self):
+        self.result = 0
+        try:
+            self.val1 = float(input("Enter base value: "))
+        except ValueError as e:
+            e = "Oops ! There seems to be a value error, check your input and try again"
+            print(e)
+        try:
+            self.val2 = float(input("Enter exponent value: "))
+        except ValueError as e:
+            e = "Oops ! There seems to be a value error, check your input and try again"
+            print(e)
+    def expo(self):
+        try:
+            self.result = float(round(self.val1 ** self.val2, 10))
+            return round(self.result, 10)
+        except Exception as e:
+            e = "There seems to be an error, check your inputs and try again"
+            print(e)
+        
 
-    if math_calculator.op == "bye":
-        break
 
-    result = None
-
-    if math_calculator.op in ["+", "-", "*", "/"]:
-        result = math_calculator.add() if math_calculator.op == "+" else \
-                 math_calculator.substract() if math_calculator.op == "-" else \
-                 math_calculator.multiply() if math_calculator.op == "*" else \
-                 math_calculator.divide()
-
-        if result is not None:
-            print("")
-            print(f"Result: {result}")
-            print("")
-
-    elif math_calculator.op in ["sin", "cos", "tan"]:
-        print(f"{math_calculator.op.upper()} of {math_calculator.val}: {getattr(math_calculator, math_calculator.op)}")
