a7e41e68c3a4947091ba9f336d38f222ad5e82ec
esteban <99847900+esteban5875@users.noreply.github.com>
2023-11-12 21:14:34 -0400

    Nuevo Proyecto

--- codigo pa el vicio/GestorV3.py
+++ codigo pa el vicio/GestorV3.py
@@ -2,60 +2,71 @@ import random
 
 class Codes:
     def __init__(self):
-        self.codes_generated = []
-    def generate_code(self):
+        self.codes_generated = set()
+    @staticmethod
+    def generate_code():
         while True:
-            self.result_code = random.randint(10000000000, 99999999999)
-            if self.result_code not in self.codes_generated:
+            result_code = random.randint(10000000000, 99999999999)
+            if result_code not in Codes.codes_generated:
+                Codes.codes_generated.add(result_code)
                 break
 
 class Product:
-    def __init__(self):
-        try:
-            self.name = str(input("Enter product name: "))
-            if self.name == "":
-                print("Product name cannot be empty --You can use spaces though--")
-        except Exception as e:
-            print(e)
-        try:
-            self.price = float(input("Enter product price: "))
-        except ValueError:
-            print("Oops ! there appears to be a value error, try again...")
-        except Exception as e:
-            e = "Oops ! there appears to be an error, try again..."
-            print(e)
-        try:
-            self.quantity = float(input("Enter product quantity: "))
-        except ValueError:
-            print("Oops ! there appears to be a value error, try again...")
-        except Exception as e:
-            e = "Oops ! there appears to be an error, try again..."
-            print(e)
-        try:
-            self.revenue = self.price * self.quantity
-        except Exception as e:
-            e = "Oops ! there appears to be an error, try again..."
-            print(e)
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.revenue = self.price  * self.quantity
+        self.code = Codes.generate_code()
             
-
 class Gestor:
     def __init__(self):
-        self.inventario = {}
+        self.inventory = {}
     
     def section_add(self):
-        self.section = input("Enter section name: ")
-        if self.section not in self.inventario:
-            self.inventario[self.seccion] = []
+        self.section = input("Enter section name: ").lower()
+        if self.section.isspace():
+            print("Section name cannot be empty")
+        if self.section not in self.inventory:
+            self.inventory[self.section] = []
         else:
             print("Section already exists")
 
     def product_add(self):
         self.section = input("In which section: ")
-        if self.section not in self.inventario:
+        if self.section not in self.inventory:
             print("Section does not exist")
+        elif self.section.isspace():
+            print("Section name cannot be empty")
         else:
-            result = Product()
-            self.inventario[self.section].append(result)
+            self.name = input("Enter product name: ")
+            if self.name.isspace():
+                print("Product name cannor be empty")
+            else:
+                while True:    
+                    try:
+                        self.price = float(input("Enter price --No currency sign--: "))
+                        break
+                    except ValueError:
+                        print("Oops ! There seems to be a value error, try again...")
+                    except Exception as e:
+                        e = "There seems to be an error, try again"
+                        print(e)
+                while True:
+                    try:
+                        self.quantity = int(input("Enter product quantity: "))
+                        break
+                    except ValueError:
+                        print("Oops ! There seems to be a value error, try again...")
+                    except Exception as e:
+                        e = "There seems to be an error, try again"
+                        print(e)
+            product = Product(self.name, self.price, self.quantity, self.revenue, Codes.self.code)
+            self.inventory[self.section].append(product)
+                    
+    
+    class Update:
+        def update_name(self):
+            pass
 
-Gestor.section_add()
-Gestor.product_add()
+
