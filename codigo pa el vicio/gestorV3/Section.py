d8c64506a5f2fbb3d53cdf6073c1d43f1d0fe186
EcJuega <ecjuega@gmail.com>
2023-11-23 18:31:05 -0400

    Curso Flask

--- codigo pa el vicio/gestorV3/Section.py
+++ codigo pa el vicio/gestorV3/Section.py
@@ -1,12 +1,31 @@
+from Product_minimal import *
+
+class SectionDoesNotExist(KeyError):
+    def __init__(self, section_entered):
+        self.section_entered = section_entered
+        self.message = f"The section entered does not exist."
+
+
 
 class Section:
+    
+    inventory = {}
+
     def __init__(self, name):
         self.name = name
         self.product_list = []
     
-    def add_product():
-        pass
+    def add_section(section_name):
+        new_section = Section(name=section_name)
+        Section.inventory[section_name] = new_section.product_list
     
+    def add_product(self, product_name, product_price, product_quantity, section):
+        if section not in Section.inventory:
+            raise SectionDoesNotExist(section)
+        else:
+            product = Product(name=product_name, price=product_price, quantity=product_quantity)
+            Section.inventory[section].append(product)
+            
     def delete_product():
         pass
 
