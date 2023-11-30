abafa3dd1d4274562c0c6190872b031dcfbae837
esteban <99847900+esteban5875@users.noreply.github.com>
2023-11-29 20:06:16 -0500

    Add files via upload

---
+++ Practice-Projects/Pokedex/pokemon.py
@@ -0,0 +1,20 @@
+from math import sqrt
+
+class pokemon():
+    def __init__(self, pokemon_xp, attacks, species, name, types, HP, defense, speed):
+        self.name = name
+        self.species = species
+        self.pokemon_xp = pokemon_xp
+        self.types = types
+        self.__pokemon_level = 64
+        self.HP = HP
+        self.defense = defense
+        self.speed = speed
+        self.attacks = attacks
+
+class attack():
+    def __init__(self, attack_name, damage, level, type):
+        self.attack_name = attack_name
+        self.attack_damage = damage
+        self.level_required = level
+        self.type = type
